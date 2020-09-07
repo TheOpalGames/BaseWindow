9e4773fa21074b5a019a2f43c8057f65e1e98f20
Hallowizer <40325350+Hallowizer@users.noreply.github.com>
2020-09-06 19:28:53 -0700

    Added libgdx stuff

---
+++ src/net/theopalgames/basewindow/InputVector.java
@@ -0,0 +1,12 @@
+package net.theopalgames.basewindow;
+
+public class InputVector extends InputPoint {
+    public double startX;
+    public double startY;
+
+    public InputVector(double x, double y) {
+        super(x, y);
+        this.startX = x;
+        this.startY = y;
+    }
+}
