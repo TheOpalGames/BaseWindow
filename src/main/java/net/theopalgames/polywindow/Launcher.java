7e52db511cd593616668561e0fe515d5858b2c88
Hallowizer <40325350+Hallowizer@users.noreply.github.com>
2020-10-06 17:43:46 -0700

    Javadoc on the launcher.

--- src/main/java/net/theopalgames/polywindow/Launcher.java
+++ src/main/java/net/theopalgames/polywindow/Launcher.java
@@ -5,14 +5,37 @@ import net.theopalgames.polywindow.swing.SwingWindow;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 import java.util.jar.Manifest;
 
-public class Launcher {
+/**
+ * Launches PolyWindow on the desktop.
+ *
+ * @author hallowizer
+ */
+public enum Launcher {
+    ;
+    
+    /**
+     * The default horizontal length of a new window.
+     */
     public static final int DEFAULT_X = 1400;
+    
+    /**
+     * The default height of a new window.
+     */
     public static final int DEFAULT_Y = 900;
+    
+    // what is this? render distance?
     public static final int DEFAULT_Z = 1000;
-
+    
+    /**
+     * Main function, please don't call.
+     *
+     * @param args The command line arguments.
+     * @throws Exception If something goes wrong somewhere.
+     */
     public static void main(String[] args) throws Exception{
         FileManager files = new ComputerFileManager();
         Game game = createGame(files);
@@ -35,9 +58,21 @@ public class Launcher {
 
         window.run();
     }
-
+    
+    /**
+     * Creates a game from a {@link FileManager} and calls its preInit function.
+     *
+     * @param files The file manager to use.
+     * @return An instance of the game.
+     * @throws IOException If an error occurs reading the manifest for the game.
+     * @throws ClassNotFoundException If the game class doesn't exist.
+     * @throws IllegalAccessException Shouldn't throw.
+     * @throws InstantiationException Shouldn't throw.
+     * @throws NoSuchMethodException If the game class doesn't have a default constructor.
+     * @throws InvocationTargetException If the constructor throws an exception.
+     */
     @SuppressWarnings("unchecked")
-    public static Game createGame(FileManager files) throws IOException, ClassNotFoundException, IllegalAccessException, InstantiationException {
+    public static Game createGame(FileManager files) throws IOException, ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
         String gameLocation;
 
         try (InputStream data = files.getInternalFileContents("META-INF/MANIFEST.MF")) {
@@ -49,7 +84,7 @@ public class Launcher {
             throw new IllegalStateException("No Game class found!");
 
         Class<? extends Game> clazz = (Class<? extends Game>) Class.forName(gameLocation);
-        Game game = clazz.newInstance();
+        Game game = clazz.getDeclaredConstructor().newInstance();
         game.preInit(files);
 
         return game;
